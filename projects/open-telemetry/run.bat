5b47169de30fc147d7a43e7044b079c4d92d5741
Sean Keery <skibum55@hotmail.com>
2026-02-10 17:09:47 +0000

    dotnet commands

---
+++ projects/open-telemetry/run.bat
@@ -0,0 +1,5 @@
+dotnet run open-telemetry-1
+dotnet run open-telemetry-2
+dotnet run open-telemetry-3
+dotnet run open-telemetry-4
+dotnet run open-telemetry-5
---
+++ projects/open-telemetry/test.bat
@@ -0,0 +1,5 @@
+dotnet test open-telemetry-1
+dotnet test open-telemetry-2
+dotnet test open-telemetry-3
+dotnet test open-telemetry-4
+dotnet test open-telemetry-5
